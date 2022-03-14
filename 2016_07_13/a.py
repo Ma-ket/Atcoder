e08120c000d5a6146337edd08806efeb1698cd23
浜田真人 <makoto@H--M.local>
2022-03-14 22:37:39 +0900

    2016/07/13の問題を解いてる途中

---
+++ 2016_07_13/a.py
@@ -0,0 +1,8 @@
+input_num = list(map(int, input().split()))
+
+if (input_num == [5, 7, 5] \
+    or input_num == [7, 5, 5] \
+    or input_num == [5, 5, 7]):
+    print('YES')
+else :
+    print('NO')
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Atcoder
-AtCoderに向けたソースコードを保管して置くために作成しました。
+# このディレクトリの目的
+このディレクトリは、AtCoderに向けたソースコードを保管して置くために作成しました。
 
 ユーザ：
 https://atcoder.jp/users/Market
